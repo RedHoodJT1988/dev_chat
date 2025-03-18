98dc67ed747593d93943a1eb83a8484c935a03cf
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-18 10:12:10 -0500

    basic signup button rendering for registration page

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,9 +1,18 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+use dioxus_router::{Route, Router};
 use fermi::use_init_atom_root;
+use console_error_panic_hook;
+
+use crate::prelude::*;
 
 pub fn App(cx: Scope) -> Element {
+    console_error_panic_hook::set_once();
     use_init_atom_root(cx);
-    None
+    cx.render(rsx! {
+        Router {
+            Route { to: page::ACCOUNT_REGISTER, page::Register {} },
+        }
+    })
 }
